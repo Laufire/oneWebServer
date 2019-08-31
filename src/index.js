56bd8b973ae05ce1afc42256fff759f59431b97b
laufire technologies <laufiretechnologies@gmail.com>
2019-09-01 00:01:07 +0530

    Integrated with oneRouter.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 /* The main entry. */
+
 import express from 'express';
 import bodyParser from './modules/bodyParser';
 import requestParser from './modules/requestParser';
+import oneRouter from '@laufire/one-router';
 
 /* Delegates */
 const app = express();
 
 /* Exports */
-export default (config, core) => {
-	const router = core ? core.parse(config.router, 'router') : config.router;
+export default (config, one) => {
+	one.types.router = one.types.router || oneRouter;
+
+	const router = one.parse({ router: config.router });
 	const { port, bodyFormat, idField = 'id' } = config;
 
 	app.use(bodyParser(bodyFormat));
 
-	app.all('/*', async (req, resp) => {
-		resp.end(await router(requestParser(req, idField)));
-	});
+	app.all('/*', async (req, resp) =>
+		resp.end(await router(requestParser(req, idField)).router.toString()));
 
 	app.listen(port);
 };
